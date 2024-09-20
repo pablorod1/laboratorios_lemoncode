8bb4e1971f1e254b92d01915439449ac603236e9
Pablo Rodriguez Albarran <pablorodriguezalbarran2000@gmail.com>
2024-09-20 15:33:22 +0200

    Entrega Final

--- modulo_2/javascript-sandbox/src/main.js
+++ modulo_2/javascript-sandbox/src/main.js
@@ -1,3 +1,9 @@
 import "./style.css";
 
-console.log("Hello from main");
+const amigos = 6;
+const ticket = 120;
+const bebidas = 18;
+
+const totalADividir = ticket - bebidas;
+const totalPorAmigo = totalADividir / amigos;
+console.log(`Precio por comensal ${totalPorAmigo}€`);
