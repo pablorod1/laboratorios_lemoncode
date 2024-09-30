4f5021f602f33406dabe3093b00bdbe1a76d0e92
Pablo Rodriguez Albarran <pablorodriguezalbarran2000@gmail.com>
2024-09-30 09:55:40 +0200

    remove ts-sandbox

--- modulo_8/typescript-sandbox/src/ui.ts
+++
@@ -1,8 +0,0 @@
-import { Carta, Tablero, tablero } from "./model";
-import { iniciarPartida } from "./motor";
-
-const iniciarButton = document.getElementById("iniciar-button");
-
-if (iniciarButton && iniciarButton instanceof HTMLButtonElement) {
-  iniciarButton.addEventListener("click", () => iniciarPartida(tablero));
-}
